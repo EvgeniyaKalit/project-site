7b5610f73385e84ff64f7a6e201a8af149bccb79
EvaKalit <git@github.com:EvgeniyaKalit/site-fish.git>
2020-03-22 13:37:55 +0500

    refact: rewrite modal in file main.js using jQuery

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-document.addEventListener("DOMContentLoaded", function(event) { 
+/*document.addEventListener("DOMContentLoaded", function(event) { 
   const modal = document.querySelector('.modal');
   const modalBtn = document.querySelectorAll('[data-toggle=modal]');
   const closeBtn = document.querySelector('.modal__close');
@@ -10,20 +10,30 @@ document.addEventListener("DOMContentLoaded", function(event) {
     
   closeBtn.addEventListener('click', swithModal);
     
-    })
+    })*/
+    
+
+  $(document).ready(function () {
+    
+ 
+     
+  var modal = $('.modal'),
+      modalBtn = $('[data-toggle=modal]'),
+      closeBtn = $('.modal__close');
    
+  modalBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+    });
+  
+  closeBtn.on('click', function () {
+      modal.toggleClass('modal--visible');
+    });
     
+  });
+      
+   
+  
  
     
   
-    
 
-  
- 
-  
-    
- 
-  
-
-  
-  
